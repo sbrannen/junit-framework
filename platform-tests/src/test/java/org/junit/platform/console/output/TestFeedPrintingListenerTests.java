xjc90s/junit5

Deprecate `OutputDirectoryProvider` in favor of `OutputDirectoryCreator` (#4924)
Resolves #4923.

platform-tests/src/test/java/org/junit/platform/console/output/TestFeedPrintingListenerTests.java
@@ -12,7 +12,7 @@ package org.junit.platform.console.output;
 
 import static org.junit.jupiter.api.Assertions.assertLinesMatch;
 import static org.junit.platform.commons.util.CollectionUtils.getOnlyElement;
-import static org.junit.platform.launcher.core.OutputDirectoryProviders.dummyOutputDirectoryProvider;
+import static org.junit.platform.launcher.core.OutputDirectoryCreators.dummyOutputDirectoryCreator;
 import static org.mockito.Mockito.mock;
 
 import java.io.PrintWriter;
@@ -45,7 +45,7 @@ public class TestFeedPrintingListenerTests {
 			"%c ool test");
 		engineDescriptor.addChild(testDescriptor);
 
-		testPlan = TestPlan.from(true, Set.of(engineDescriptor), mock(), dummyOutputDirectoryProvider());
+		testPlan = TestPlan.from(true, Set.of(engineDescriptor), mock(), dummyOutputDirectoryCreator());
 		testIdentifier = testPlan.getTestIdentifier(testDescriptor.getUniqueId());
 
 		listener.testPlanExecutionStarted(testPlan);

sbrannen/junit-framework

(cherry picked from commit e47c2538a5bbf45595f25ee86f6c20c6ac6a6e7a)

platform-tests/src/test/java/org/junit/platform/console/output/TestFeedPrintingListenerTests.java
@@ -12,7 +12,7 @@ package org.junit.platform.console.output;
 
 import static org.junit.jupiter.api.Assertions.assertLinesMatch;
 import static org.junit.platform.commons.util.CollectionUtils.getOnlyElement;
-import static org.junit.platform.launcher.core.OutputDirectoryProviders.dummyOutputDirectoryProvider;
+import static org.junit.platform.launcher.core.OutputDirectoryCreators.dummyOutputDirectoryCreator;
 import static org.mockito.Mockito.mock;
 
 import java.io.PrintWriter;
@@ -45,7 +45,7 @@ public class TestFeedPrintingListenerTests {
 			"%c ool test");
 		engineDescriptor.addChild(testDescriptor);
 
-		testPlan = TestPlan.from(true, Collections.singleton(engineDescriptor), mock(), dummyOutputDirectoryProvider());
+		testPlan = TestPlan.from(true, Collections.singleton(engineDescriptor), mock(), dummyOutputDirectoryCreator());
 		testIdentifier = testPlan.getTestIdentifier(testDescriptor.getUniqueId());
 
 		listener.testPlanExecutionStarted(testPlan);