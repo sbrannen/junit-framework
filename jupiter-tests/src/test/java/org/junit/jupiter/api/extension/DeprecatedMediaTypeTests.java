xjc90s/junit5

Relocate MediaType to jupiter.api package and deprecate existing class
org.junit.jupiter.api.extension.MediaType currently causes cycles
between the `api` and `api.extension` packages in junit-jupiter-api.

Since the junit-jupiter-api artifact constitutes the public API for
JUnit Jupiter, we have traditionally not allowed package cycles within
that artifact.

Thus, in order to reduce package cycles and improve the quality of the
code base, this commit introduces a new MediaType in the
org.junit.jupiter.api package. Note that the existing MediaType now
extends the new MediaType.

To encourage users to migrate to the new supported MediaType this
commit also introduces the following deprecations.

* `org.junit.jupiter.api.extension.MediaType` is now deprecated in
  favor of the new `org.junit.jupiter.api.MediaType`.

* The `publishFile(...)` methods in `TestReporter` which accept an
  `org.junit.jupiter.api.extension.MediaType` are now deprecated in
  favor of new variants which accept an
  `org.junit.jupiter.api.MediaType`.

* The `publishFile(...)` method in `ExtensionContext` which accepts an
  `org.junit.jupiter.api.extension.MediaType` is now deprecated in
  favor of a new variant which accepts an
  `org.junit.jupiter.api.MediaType`.

Furthermore, we plan to remove the deprecated MediaType in JUnit 6.2.

See #4876
Closes #4886
Closes #4916

jupiter-tests/src/test/java/org/junit/jupiter/api/extension/DeprecatedMediaTypeTests.java
@@ -25,9 +25,13 @@ import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 /**
- * Unit tests for {@link MediaType}.
+ * Unit tests for the deprecated {@link MediaType}.
+ *
+ * @see org.junit.jupiter.api.MediaTypeTests
+ * @see MediaTypeInteroperabilityTests
  */
-class MediaTypeTests {
+@SuppressWarnings("removal")
+class DeprecatedMediaTypeTests {
 
 	@Test
 	void equals() {

jupiter-tests/src/test/java/org/junit/jupiter/api/extension/MediaTypeInteroperabilityTests.java
@@ -1,0 +1,38 @@
+/*
+ * Copyright 2015-2025 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+
+package org.junit.jupiter.api.extension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.EqualsAndHashCodeAssertions.assertEqualsAndHashCode;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Interoperability tests for {@link org.junit.jupiter.api.MediaType} and the
+ * deprecated {@link org.junit.jupiter.api.extension.MediaType}.
+ *
+ * @since 6.0
+ * @see DeprecatedMediaTypeTests
+ */
+class MediaTypeInteroperabilityTests {
+
+	@Test
+	@SuppressWarnings("removal")
+	void newAndDeprecatedMediaTypesAreLogicallyEquivalent() {
+		var mediaType = org.junit.jupiter.api.MediaType.TEXT_PLAIN_UTF_8;
+		var deprecatedMediaType = MediaType.TEXT_PLAIN_UTF_8;
+		var differentMediaType = MediaType.TEXT_PLAIN;
+
+		assertThat(mediaType.getClass()).isNotEqualTo(deprecatedMediaType.getClass());
+		assertEqualsAndHashCode(mediaType, deprecatedMediaType, differentMediaType);
+	}
+
+}

sbrannen/junit-framework

jupiter-tests/src/test/java/org/junit/jupiter/api/extension/DeprecatedMediaTypeTests.java
@@ -24,9 +24,13 @@ import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 /**
- * Unit tests for {@link MediaType}.
+ * Unit tests for the deprecated {@link MediaType}.
+ *
+ * @see org.junit.jupiter.api.MediaTypeTests
+ * @see MediaTypeInteroperabilityTests
  */
-class MediaTypeTests {
+@SuppressWarnings("deprecation")
+class DeprecatedMediaTypeTests {
 
 	@Test
 	void equals() {

jupiter-tests/src/test/java/org/junit/jupiter/api/extension/MediaTypeInteroperabilityTests.java
@@ -1,0 +1,38 @@
+/*
+ * Copyright 2015-2025 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+
+package org.junit.jupiter.api.extension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.EqualsAndHashCodeAssertions.assertEqualsAndHashCode;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Interoperability tests for {@link org.junit.jupiter.api.MediaType} and the
+ * deprecated {@link org.junit.jupiter.api.extension.MediaType}.
+ *
+ * @since 6.0
+ * @see DeprecatedMediaTypeTests
+ */
+class MediaTypeInteroperabilityTests {
+
+	@Test
+	@SuppressWarnings("deprecation")
+	void newAndDeprecatedMediaTypesAreLogicallyEquivalent() {
+		var mediaType = org.junit.jupiter.api.MediaType.TEXT_PLAIN_UTF_8;
+		var deprecatedMediaType = MediaType.TEXT_PLAIN_UTF_8;
+		var differentMediaType = MediaType.TEXT_PLAIN;
+
+		assertThat(mediaType.getClass()).isNotEqualTo(deprecatedMediaType.getClass());
+		assertEqualsAndHashCode(mediaType, deprecatedMediaType, differentMediaType);
+	}
+
+}